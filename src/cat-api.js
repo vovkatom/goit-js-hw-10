38f79085da63df39377b6c830b7dbec5e633034f
vovkatom <36505066+vovkatom@users.noreply.github.com>
2023-09-23 16:06:51 +0300

    1

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,10 +1,15 @@
+// Імпорт модуля Axios для виконання HTTP-запитів
 import axios from 'axios';
 
+// URL і ключ доступу API
 const URL = 'https://api.thecatapi.com/v1/';
 const API_KEY =
   'live_Uczt18D595C6qQt0FV6BIE3pO0lPgMNJbt2vi6yynq9BhPO9UHt9Z5XcuGnVcxuF';
+
+// Встановлення заголовка x-api-key для доступу до API
 axios.defaults.headers.common['x-api-key'] = API_KEY;
 
+// Функція для отримання списку порід котів
 function fetchBreeds() {
   const BREEDS_URL = `${URL}breeds`;
   return axios.get(BREEDS_URL).then(res => {
@@ -14,11 +19,16 @@ function fetchBreeds() {
     return res.data;
   });
 }
+
+// Функція для отримання інформації про кота за ID породи
 function fetchCatByBreed(breedId) {
   const IMAGES_URL = `${URL}images/search`;
+  
+  // Створення параметрів запиту з ID породи
   const params = new URLSearchParams({
     breed_ids: breedId,
   });
+  
   return axios.get(`${IMAGES_URL}?${params}`).then(res => {
     if (res.status !== 200) {
       throw new Error(res.status);
@@ -26,4 +36,6 @@ function fetchCatByBreed(breedId) {
     return res.data[0];
   });
 }
+
+// Експорт функцій для використання в інших частинах програми
 export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,43 +1,54 @@
-// import SlimSelect from 'slim-select';
-// import 'slim-select/dist/slimselect.css';
-import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
-import { Loading } from 'notiflix/build/notiflix-loading-aio';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// Імпорт необхідних модулів та стилів
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js'; // Імпорт функцій для отримання інформації про котів з власного API
+import { Loading } from 'notiflix/build/notiflix-loading-aio'; // Імпорт модуля Loading з бібліотеки Notiflix для відображення завантаження
+import { Notify } from 'notiflix/build/notiflix-notify-aio'; // Імпорт модуля Notify з бібліотеки Notiflix для відображення повідомлень
 
+// Створення посилань на HTML-елементи за їх класами
 const refs = {
-  select: document.querySelector('.breed-select'),
-  loader: document.querySelector('.loader'),
-  err: document.querySelector('.error'),
-  catCard: document.querySelector('.cat-info'),
+  select: document.querySelector('.breed-select'), // Вибір породи кота
+  loader: document.querySelector('.loader'), // Елемент завантаження
+  err: document.querySelector('.error'), // Елемент для відображення повідомлення про помилку
+  catCard: document.querySelector('.cat-info'), // Карта кота з інформацією
 };
 
+// Початкове приховання елементів на сторінці
 refs.loader.style.display = 'none';
 refs.err.style.display = 'none';
 refs.select.style.display = 'none';
 refs.catCard.style.display = 'none';
 
+// Відображення анімованих крапок завантаження за допомогою Notiflix
 Loading.dots({
   svgColor: '#5897fb',
   svgSize: '130px',
   messageFontSize: '30px',
 });
 
+// Запит на отримання списку порід котів та обробка результату
 fetchBreeds()
   .then(data => {
+    // Показати вибір породи та приховати елементи завантаження
     refs.select.style.display = 'flex';
     refs.loader.style.display = 'none';
 
-    createMarkupOptins(data);
+    // Створення опцій для вибору породи
+    createMarkupOptions(data);
+
+    // Ініціалізація плагіну SlimSelect для стильного вибору
     new SlimSelect({
       select: refs.select,
     });
   })
   .catch(err => {
+    // Відображення повідомлення про помилку за допомогою Notiflix
     Notify.failure(refs.err.textContent);
   })
   .finally(result => Loading.remove());
 
-function createMarkupOptins(arr) {
+// Функція для створення опцій вибору породи
+function createMarkupOptions(arr) {
   return arr
     .map(({ id, name }) => {
       console.log({ id, name });
@@ -48,26 +59,32 @@ function createMarkupOptins(arr) {
     .join('');
 }
 
+// Обробка події вибору породи кота
 refs.select.addEventListener('change', e => {
   const id = e.target.value;
 
+  // Відображення анімованих крапок завантаження за допомогою Notiflix
   Loading.dots({
     svgColor: '#5897fb',
     svgSize: '130px',
     messageFontSize: '30px',
   });
 
+  // Запит на отримання інформації про кота за обраною породою
   fetchCatByBreed(id)
     .then(catInfo => {
+      // Показати карту кота та відобразити інформацію
       refs.catCard.style.display = 'flex';
       createMarkupCards(catInfo);
     })
     .catch(err => {
+      // Відображення повідомлення про помилку за допомогою Notiflix
       Notify.failure(refs.err.textContent);
     })
     .finally(result => Loading.remove());
 });
 
+// Функція для створення інформаційної карти про кота
 function createMarkupCards(data) {
   const {
     breeds: { name, description, temperament },
@@ -79,16 +96,16 @@ function createMarkupCards(data) {
        <div class="cat-right">
       <h1 class="name">${name}</h1>
       <p class="description">${description}</p>
-      <p class="temperament"><span class="temperament-span">Temperament:</span> ${temperament}</p>    
+      <p class="temperament"><span class="temperament-span">Темперамент:</span> ${temperament}</p>    
       </div>`;
 
-  // const card = `
+  refs.catCard.innerHTML = card;
+}
+
+// const card = `
   //     <img class="cat-img" src="${data.url}" alt="${data.breeds[0].name}"  >
   //      <div class="cat-right">
   //     <h1 class="name">${data.breeds[0].name}</h1>
   //     <p class="description">${data.breeds[0].description}</p>
   //     <p class="temperament"><span class="temperament-span">Temperament:</span> ${data.breeds[0].temperament}</p>
   //     </div>`;
-
-  refs.catCard.innerHTML = card;
-}
